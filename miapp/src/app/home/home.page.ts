61784dc15544e3f919bb5d36d70544f946bc4d81
JordinRosario <jordinjose2003@gmail.com>
2022-11-25 13:51:52 -0500

    Cambio en la pagina principal

--- miapp/src/app/home/home.page.ts
+++ miapp/src/app/home/home.page.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+
+  todoList = [{
+    itemName : 'Programando',
+    itemDeuDate: '24-11-2022',
+    itemPriority : 'Alta',
+    itemCategory : 'Proyecto'
+
+  },
+
+  {
+    itemName : 'Diseñando',
+    itemDeuDate: '28-11-2022',
+    itemPriority : 'Baja',
+    itemCategory : 'Diseño'
+
+  },
+  {
+    itemName : 'Comprando',
+    itemDeuDate: '29-11-2022',
+    itemPriority : 'Intermedio',
+    itemCategory : 'Personal'
+
+  },
+]
+
+today : number = Date.now();
   constructor() {}
 
 }
